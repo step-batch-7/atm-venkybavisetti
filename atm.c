15ab37a62632e858ca6d2153b5c8af528bc80224
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-04-08 00:34:22 +0530

    implemented the get_money function

--- atm.c
+++ atm.c
@@ -7,7 +7,7 @@ unsigned int get_money(unsigned short int money)
   int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned int denomination_count = 0x0;
 
-  for (int index = 0; index < 8; index++)
+  for (int index = 0; index < 8 && balance > 31999; index++)
   {
     denomination_count <<= 4;
     denomination_count = denomination_count | (balance / denominations[index]);
